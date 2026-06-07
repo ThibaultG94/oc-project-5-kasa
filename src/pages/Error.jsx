a07aca1a813229f64d4e748f29188985c03767e7
Thibault Guilhem <thibault.guilhem@gmail.com>
2026-06-07 21:23:09 +0200

    add 404 page with React Router return link matching Figma layout

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,9 +1,19 @@
+import { Link } from "react-router";
+
 function Error() {
   return (
-    <>
-      <h1>404</h1>
-      <p>Oups! La page que vous demandez n'existe pas.</p>
-    </>
+    <section className="error" aria-labelledby="error-title">
+      <h1 id="error-title" className="error__code">
+        404
+      </h1>
+      <p className="error__message">
+        Oups! La page que <br className="error__break" />
+        vous demandez n'existe pas.
+      </p>
+      <Link to="/" className="error__link">
+        Retourner sur la page d&rsquo;accueil
+      </Link>
+    </section>
   );
 }
 
